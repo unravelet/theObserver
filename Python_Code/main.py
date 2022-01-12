059f441688995fbead5ef647a6d15bfe91fc416f
yqni13 <yqni13@protonmail.com>
2022-01-12 17:57:33 +0100

    update#1
    including python main file

---
+++ Python_Code/main.py
@@ -0,0 +1,278 @@
+import cv2
+import imutils
+from imutils.video import VideoStream
+from imutils.video import FPS
+from threading import Thread
+import time
+from datetime import datetime, time
+import numpy as np
+#import mediapipe as mp
+import argparse
+
+
+
+
+# cap = cv2.VideoCapture(1)
+#
+# while True:
+#     ret, frame = cap.read()
+#     cv2.imshow('frame', frame)
+#     if cv2.waitKey(1) == ord('q'):
+#         break
+
+
+
+
+#######################################################################################
+## first try of recognizing people from video
+
+# def detect(frame):
+#     bounding_box_cordinates, weights = HOGCV.detectMultiScale(frame, winStride=(4, 4), padding=(8, 8), scale=1.03)
+#
+#     person = 1
+#     for x, y, w, h in bounding_box_cordinates:
+#         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+#         cv2.putText(frame, f'person {person}', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+#         person += 1
+#
+#     cv2.putText(frame, 'Status : Detecting ', (40, 40), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 0, 0), 2)
+#     cv2.putText(frame, f'Total Persons : {person - 1}', (40, 70), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 0, 0), 2)
+#     cv2.imshow('theObserver_demo_data', frame)
+#     return frame
+#
+#
+# def humanDetector():
+#     video_path = "videos/video2.mp4"
+#
+#     #writer = cv2.VideoWriter(video_path, cv2.VideoWriter_fourcc(*'mp4v'), 30, (800, 600))
+#     writer = None
+#     if video_path is not None:
+#         print('[INFO] Opening Video from path.')
+#         detectByPathVideo(video_path, writer)
+#     else:
+#         print("ERROR: File not Found")
+#         return False
+#
+#
+# def detectByPathVideo(path, writer):
+#     video = cv2.VideoCapture(path)
+#     fps = int(video.get(cv2.CAP_PROP_FPS))
+#     print("fps of current video file: ", fps)
+#     check, frame = video.read()
+#     if not check:
+#         print('Video Not Found. Please Enter a Valid Path (Full path of Video Should be Provided).')
+#         return
+#     print('Detecting people...')
+#     while True:
+#         # check is True if reading was successful
+#         check, frame = video.read()
+#         #frame = imutils.resize(frame, width=min(800, frame.shape[1]))
+#         frame = detect(frame)
+#
+#         if writer is not None:
+#             writer.write(frame)
+#
+#         if cv2.waitKey(1) & 0xFF == ord('q') or not check:
+#             video.release()
+#             cv2.destroyAllWindows()
+#             break
+#
+#
+# HOGCV = cv2.HOGDescriptor()
+# HOGCV.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())  #pre-trained model human detection
+#
+# humanDetector()
+
+
+#######################################################################################
+## open and run video from given path
+
+# video = cv2.VideoCapture("videos/video2.mp4")
+# fps = int(video.get(cv2.CAP_PROP_FPS))
+# print("fps of current video file: ", fps)
+#
+# if not video.isOpened():
+#     print("ERROR: File not found.")
+#
+# ret, frame = video.read()
+# while True:
+#     #check if frames are returning
+#     ret, frame = video.read()
+#
+#     #opencv automatically speeds up video, needs to slow down to original pace
+#
+#
+#     #displaying the frames of video
+#     cv2.imshow('theObserver_demo_data', frame)
+#
+#     #quit video before end with command 'q'
+#     if cv2.waitKey(1) & 0xFF == ord('q') or not ret:
+#         video.release()
+#         cv2.destroyAllWindows()
+#         break
+
+#######################################################################################
+## Example code from geeksforgeeks
+## adapt with code from: https://nrsyed.com/2018/07/05/multithreading-with-opencv-python-to-improve-video-processing-performance/
+
+
+
+class CountsPerSec:
+    """
+    Class that tracks the number of occurrences ("counts") of an
+    arbitrary event and returns the frequency in occurrences
+    (counts) per second. The caller must increment the count.
+    """
+
+    def __init__(self):
+        self._start_time = None
+        self._num_occurrences = 0
+
+    def start(self):
+        self._start_time = datetime.now()
+        return self
+
+    def increment(self):
+        self._num_occurrences += 1
+
+    def countsPerSec(self):
+        elapsed_time = (datetime.now() - self._start_time).total_seconds()
+        return self._num_occurrences / elapsed_time
+
+#####
+def putIterationsPerSec(frame, iterations_per_sec):
+    """
+    Add iterations per second text to lower-left corner of a frame.
+    """
+
+    cv2.putText(frame, "{:.0f} iterations/sec".format(iterations_per_sec),
+        (10, 450), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255))
+    return frame
+
+######
+
+
+class VideoGet:
+    """
+    Class that continuously gets frames from a VideoCapture object
+    with a dedicated thread.
+    """
+
+    def __init__(self, src=0):
+        self.stream = cv2.VideoCapture(src)
+        (self.grabbed, self.frame) = self.stream.read()
+        self.stopped = False
+
+    def start(self):
+        Thread(target=self.get, args=()).start()
+        return self
+
+    def get(self):
+        while not self.stopped:
+            if not self.grabbed:
+                self.stop()
+            else:
+                (self.grabbed, self.frame) = self.stream.read()
+
+    def stop(self):
+        self.stopped = True
+
+#####
+
+
+class VideoShow:
+    """
+    Class that continuously shows a frame using a dedicated thread.
+    """
+
+    def __init__(self, frame=None):
+        self.frame = frame
+        self.stopped = False
+
+    def start(self):
+        Thread(target=self.show, args=()).start()
+        return self
+
+    def show(self):
+        while not self.stopped:
+            cv2.imshow("Video", self.frame)
+            if cv2.waitKey(1) == ord("q"):
+                self.stopped = True
+
+    def stop(self):
+        self.stopped = True
+
+######
+
+def threadBoth(source='videos/video2.mp4'):
+    """
+    Dedicated thread for grabbing video frames with VideoGet object.
+    Dedicated thread for showing video frames with VideoShow object.
+    Main thread serves only to pass frames between VideoGet and
+    VideoShow objects/threads.
+    """
+
+    video_getter = VideoGet(source).start()
+    video_shower = VideoShow(video_getter.frame).start()
+    cps = CountsPerSec().start()
+
+    while True:
+        if video_getter.stopped or video_shower.stopped:
+            video_shower.stop()
+            video_getter.stop()
+            break
+
+        frame = video_getter.frame
+        frame = putIterationsPerSec(frame, cps.countsPerSec())
+        video_shower.frame = frame
+        cps.increment()
+
+
+threadBoth()
+
+
+######
+
+# Initializing the HOG person
+# detector
+# hog = cv2.HOGDescriptor()
+# hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+#
+# cap = cv2.VideoCapture('videos/video2.mp4')
+# if not cap.isOpened():
+#   print("Error. Source file not found.")
+# else:
+#     fps = int(cap.get(cv2.CAP_PROP_FPS))
+#     print("fps of current video file: ", fps)
+#
+# while cap.isOpened():
+#     # Reading the video stream
+#     ret, image = cap.read()
+#     if ret:
+#         image = imutils.resize(image,
+#                                width=min(400, image.shape[1]))
+#
+#         # Detecting all the regions
+#         # in the Image that has a
+#         # pedestrians inside it
+#         (regions, _) = hog.detectMultiScale(image,
+#                                             winStride=(4, 4),
+#                                             padding=(4, 4),
+#                                             scale=1.05)
+#
+#         # Drawing the regions in the
+#         # Image
+#         for (x, y, w, h) in regions:
+#             cv2.rectangle(image, (x, y),
+#                           (x + w, y + h),
+#                           (0, 0, 255), 2)
+#
+#         # Showing the output Image
+#         cv2.imshow("Image", image)
+#         if cv2.waitKey(1) & 0xFF == ord('q') or not ret:
+#             break
+#     else:
+#         break
+#
+# cap.release()
+# cv2.destroyAllWindows()
