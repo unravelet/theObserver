a31f30b012d99d1eb0d5c44d0150fa1a8d701eda
yqni13 <65848806+yqni13@users.noreply.github.com>
2022-01-13 15:04:20 +0100

    deleted old unused code

--- Python_Code/main.py
+++ Python_Code/main.py
@@ -9,80 +9,51 @@ import numpy as np
 #import mediapipe as mp
 import argparse
 
-
-
-
-# cap = cv2.VideoCapture(1)
-#
-# while True:
-#     ret, frame = cap.read()
-#     cv2.imshow('frame', frame)
-#     if cv2.waitKey(1) == ord('q'):
-#         break
-
-
-
-
 #######################################################################################
-# # first try of recognizing people from video
-#
-# def detect(frame):
-#     bounding_box_cordinates, weights = HOGCV.detectMultiScale(frame, winStride=(4, 4), padding=(8, 8), scale=1.03)
-#
-#     person = 1
-#     for x, y, w, h in bounding_box_cordinates:
-#         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-#         cv2.putText(frame, f'person {person}', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
-#         person += 1
-#
-#     cv2.putText(frame, 'Status : Detecting ', (40, 40), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 0, 0), 2)
-#     cv2.putText(frame, f'Total Persons : {person - 1}', (40, 70), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 0, 0), 2)
-#     cv2.imshow('theObserver_demo_data', frame)
-#     return frame
-#
-#
-# def humanDetector():
-#     video_path = "../res/videos/video2.mp4"
-#
-#     #writer = cv2.VideoWriter(video_path, cv2.VideoWriter_fourcc(*'mp4v'), 30, (800, 600))
-#     writer = None
-#     if video_path is not None:
-#         print('[INFO] Opening Video from path.')
-#         detectByPathVideo(video_path, writer)
-#     else:
-#         print("ERROR: File not Found")
-#         return False
-#
-#
-# def detectByPathVideo(path, writer):
-#     video = cv2.VideoCapture(path)
-#     fps = int(video.get(cv2.CAP_PROP_FPS))
-#     print("fps of current video file: ", fps)
-#     check, frame = video.read()
-#     if not check:
-#         print('Video Not Found. Please Enter a Valid Path (Full path of Video Should be Provided).')
-#         return
-#     print('Detecting people...')
-#     while True:
-#         # check is True if reading was successful
-#         check, frame = video.read()
-#         #frame = imutils.resize(frame, width=min(800, frame.shape[1]))
-#         frame = detect(frame)
-#
-#         if writer is not None:
-#             writer.write(frame)
-#
-#         if cv2.waitKey(1) & 0xFF == ord('q') or not check:
-#             video.release()
-#             cv2.destroyAllWindows()
-#             break
-#
-#
-# HOGCV = cv2.HOGDescriptor()
-# HOGCV.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())  #pre-trained model human detection
-#
-# humanDetector()
 
+
+##Initializing the HOG person
+##detector
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+cap = cv2.VideoCapture('../res/videos/video2.mp4')
+if not cap.isOpened():
+  print("Error. Source file not found.")
+else:
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    print("fps of current video file: ", fps)
+
+while cap.isOpened():
+    # Reading the video stream
+    ret, image = cap.read()
+    if ret:
+        image = imutils.resize(image,
+                               width=min(400, image.shape[1]))
+
+        # Detecting all the regions
+        # in the Image that has a
+        # pedestrians inside it
+        (regions, _) = hog.detectMultiScale(image, winStride=(4, 4), padding=(4, 4), scale=1.05)
+
+        # Drawing the regions in the
+        # Image
+        person = 1
+        for (x, y, w, h) in regions:
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            cv2.putText(image, f'person {person}', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+            person += 1
+
+        # Showing the output Image
+        cv2.putText(image, f'person {person - 1}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2)
+        cv2.imshow("Image", image)
+        if cv2.waitKey(1) & 0xFF == ord('q') or not ret:
+            break
+    else:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
 
 
 #######################################################################################
@@ -234,48 +205,3 @@ import argparse
 # threadBoth('../res/videos/video2.mp4')
 
 
-#######################################################################################
-
-##Initializing the HOG person
-##detector
-hog = cv2.HOGDescriptor()
-hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-
-cap = cv2.VideoCapture('../res/videos/video2.mp4')
-if not cap.isOpened():
-  print("Error. Source file not found.")
-else:
-    fps = int(cap.get(cv2.CAP_PROP_FPS))
-    print("fps of current video file: ", fps)
-
-while cap.isOpened():
-    # Reading the video stream
-    ret, image = cap.read()
-    if ret:
-        image = imutils.resize(image,
-                               width=min(400, image.shape[1]))
-
-        # Detecting all the regions
-        # in the Image that has a
-        # pedestrians inside it
-        (regions, _) = hog.detectMultiScale(image, winStride=(4, 4), padding=(4, 4), scale=1.05)
-
-
-        # Drawing the regions in the
-        # Image
-        person = 1
-        for (x, y, w, h) in regions:
-            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
-            cv2.putText(image, f'person {person}', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
-            person += 1
-
-        # Showing the output Image
-        cv2.putText(image, f'person {person - 1}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2)
-        cv2.imshow("Image", image)
-        if cv2.waitKey(1) & 0xFF == ord('q') or not ret:
-            break
-    else:
-        break
-
-cap.release()
-cv2.destroyAllWindows()
