57f4f34fbc997fb7e780e0b4e07fb3483ad5d7d5
Dominic Grabner <grabnerdominic@gmail.com>
2022-10-24 15:36:30 +0200

    Mqtt Class: loadConfigFromYamlFile method; logging function, start class diagram

---
+++ Python_Code/log.py
@@ -0,0 +1,15 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+c_handler = logging.StreamHandler()
+c_handler.setLevel(logging.INFO)
+c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+c_handler.setFormatter(c_format)
+logger.addHandler(c_handler)
+
+f_handler = logging.FileHandler('file.log')
+f_handler.setLevel(logging.DEBUG)
+f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+f_handler.setFormatter(f_format)
+logger.addHandler(f_handler)
--- Python_Code/mqtt.py
+++ Python_Code/mqtt.py
@@ -2,7 +2,10 @@ import ssl
 import uuid
 import yaml
 import paho.mqtt.client as paho
+import logging
+import log
 
+logger = logging.getLogger('log')
 
 class Mqtt:
     lastSend = ""
@@ -17,15 +20,23 @@ class Mqtt:
         self.client.tls_set(cert_reqs=ssl.CERT_NONE)
         self.client.tls_insecure_set(True)
 
-        print(f"Connect to {self.broker}...")
+        logger.info(f"Connect to {self.broker}...")
         self.client.connect(self.broker, self.port, 60)
-        print("Connected!")
+        logger.info("Connected!")
+
+    @classmethod
+    def loadConfigFromYamlFile(cls, file):
+        with open('config.yaml') as f:
+            config = yaml.load(f, Loader=yaml.FullLoader)
+            return cls(config["mqtt_broker"], config["mqtt_port"], config["mqtt_topic"])
 
     def send(self, text):
         if self.lastSend != text:
             self.lastSend = text
+            logger.info(f"Sending: {text}")
             self.client.publish(self.topic, text, qos=1)
             self.client.loop()
+
 
     def disconnect(self):
         self.client.loop_stop()
@@ -33,7 +44,5 @@ class Mqtt:
 
 
 if __name__ == "__main__":
-    with open('config.yaml') as f:
-        config = yaml.load(f, Loader=yaml.FullLoader)
-        client = Mqtt(config["mqtt_broker"], config["mqtt_port"], config["mqtt_topic"])
-        client.send("test string")
+    client = Mqtt.loadConfigFromYamlFile('config.yaml')
+    client.send("Hello World!")
