ee2234227f720a811cd9cce2cfd3450b2e72c4b1
Dominic Grabner <grabnerdominic@gmail.com>
2022-11-21 13:16:42 +0100

    HttpStreamer

--- Python_Code/HttpStreamer.py
+++ Python_Code/HttpStreamer.py
@@ -1,41 +1,38 @@
-from flask import Response
-from flask import Flask
-
+from flask import Flask, Response
 import cv2
-
-import Recognizer
 
 # https://towardsdatascience.com/video-streaming-in-web-browsers-with-opencv-flask-93a38846fe00
 # initialize a flask object
 
-app = Flask(__name__)
+class EndpointAction(object):
+    def __init__(self, action, status = 200, headers = {}):
+        self.action = action
+        self.status = status
+        self.headers = headers
 
-camera = cv2.VideoCapture(0)
+    def __call__(self, *args):
+        return self.action()
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+class HttpStreamer(object):
+    def __init__(self, action):
+        self.flask_app = Flask(__name__)
+        self.action = action
 
+    def run(self):
+        self.add_all_endpoints()
+        self.flask_app.run()
 
-@app.route("/video_feed")
-def video_feed():
-    # return the response generated along with the specific media
-    # type (mime type)
-    return Response(gen_frames(),
-                    mimetype="multipart/x-mixed-replace; boundary=frame")
+    def add_all_endpoints(self):
+        self.add_endpoint(endpoint="/", endpoint_name="/", handler=self.index)
+        self.add_endpoint(endpoint="/video_feed", endpoint_name="/video_feed", handler=self.video_feed)
 
-def gen_frames():
-    while True:
-        success, frame = camera.read()  # read the camera frame
-        if not success:
-            break
-        else:
-            frame = Recognizer.getImage(Recognizer.hogDetector(frame), frame)
+    def add_endpoint(self, endpoint=None, endpoint_name=None, handler=None):
+        self.flask_app.add_url_rule(endpoint, endpoint_name, EndpointAction(handler)) 
 
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+    #########
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    def index(self):
+        return Response('Hello watchers! 🕶', status=200, headers={})
+    
+    def video_feed(self):
+        return Response(self.action(), mimetype="multipart/x-mixed-replace; boundary=frame")
--- Python_Code/main.py
+++ Python_Code/main.py
@@ -4,43 +4,27 @@ import time
 
 from Logger import Logger
 from MqttSender import MqttSender
+from HttpStreamer import HttpStreamer
 import Recognizer
 
 from flask import Response
 from flask import Flask
 
-results = []
+last_frame = []
+
+def get_last_frame():
+    while True:
+        if (last_frame != []):
+            frame = Recognizer.getImage(last_frame[0], last_frame[1])
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            time.sleep(1)
 
 def start_http_server():
-    app = Flask(__name__)
-
-    @app.route('/')
-    def hello():
-        return 'Hello, World!'
-
-    @app.route("/video_feed")
-    def video_feed():
-        # return the response generated along with the specific media
-        # type (mime type)
-        return Response(gen_frames(),
-                        mimetype="multipart/x-mixed-replace; boundary=frame")
-
-    def gen_frames():
-        while True:
-            if (len(results) > 0):
-                frame = Recognizer.getImage(results[-1][0], results[-1][1])
-                ret, buffer = cv2.imencode('.jpg', frame)
-                frame = buffer.tobytes()
-                yield (b'--frame\r\n'
-                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-                time.sleep(1)
-
-    app.run(debug=True, use_reloader=False)
-    return
-    while (True): 
-        if (len(results) > 0):
-            print(len(results[-1][1]))
-        time.sleep(0.5)
+    app = HttpStreamer(get_last_frame)
+    app.run()
 
 def start_recognizer():
     camera = cv2.VideoCapture(0)
@@ -48,10 +32,8 @@ def start_recognizer():
     while(True):
         ret, image = camera.read()
         result = Recognizer.hogDetector(image.copy())
-        results.append([result, image])
+        lastframe = [result, image]
         mqttSender.send(len(result))
-        #result1 = len(result) # anzahl der besucher
-        #print (result1)
 
 logger = Logger("Main")
 logger.info("Start theObserver...")
