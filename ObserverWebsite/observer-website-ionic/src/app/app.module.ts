832422a0638de8c6e0cafc6bd6ef3f405cfcf0a0
unravelet <76056657+unravelet@users.noreply.github.com>
2022-12-20 16:33:38 +0100

    added graph, treid mqtt

--- ObserverWebsite/observer-website-ionic/src/app/app.module.ts
+++ ObserverWebsite/observer-website-ionic/src/app/app.module.ts
@@ -7,10 +7,26 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { IMqttServiceOptions, MqttModule } from "ngx-mqtt";
+import { environment as env } from '../environments/environment';
+import { EventMqttService } from './services/event.mqtt.service';
+
+
+const MQTT_SERVICE_OPTIONS: IMqttServiceOptions = {
+  hostname: env.mqtt.server,
+  port: env.mqtt.port,
+  protocol: (env.mqtt.protocol === "wss") ? "wss" : "ws",
+  path: 'theObserver/Counter',
+};
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    MqttModule.forRoot(MQTT_SERVICE_OPTIONS)],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, EventMqttService ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
